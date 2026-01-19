6f9f4341d729c8847a93d7f428d4d0eb0491871e
Devansh pandey <pandeydevansh2004@gmail.com>
2026-01-19 08:27:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+workout_model = ChatHuggingFace(llm=llm)
+diet_model = ChatHuggingFace(llm=llm)
+
+# bmi and calorie calculations
+def calculate_bmi(weight, height_cm):
+    height_m = height_cm / 100
+    return round(weight / (height_m ** 2), 2)
+
+def calorie_needs(weight, height, age, goal):
+    bmr = 10 * weight + 6.25 * height - 5 * age + 5
+    if goal == "Fat Loss":
+        return int(bmr * 1.2 - 400)
+    elif goal == "Muscle Gain":
+        return int(bmr * 1.2 + 300)
+    return int(bmr * 1.2)
+st.set_page_config(page_title="AI Fitness Planner", layout="centered")
+st.title("🏋️ AI Personalized Workout & Diet Planner")
+
+age = st.number_input("Age", 16, 40)
+height = st.number_input("Height (cm)")
+weight = st.number_input("Weight (kg)")
+goal = st.selectbox("Goal", ["Fat Loss", "Muscle Gain", "Maintain"])
+diet = st.selectbox("Diet Preference", ["Veg", "Non-Veg", "Eggitarian"])
+equipment = st.selectbox("Equipment", ["None", "Dumbbells", "Gym"])
+time = st.slider("Daily Workout Time (min)", 15, 60)
+budget = st.slider("Daily Food Budget (₹)", 100, 500)
+
+# template
+
+workout_prompt = PromptTemplate(
+    template="""
+You are an expert fitness coach.
+Create a weekly workout plan for a student.
+
+Age: {age}
+Goal: {goal}
+Equipment: {equipment}
+Daily workout time: {time} minutes
+
+Requirements:
+- Student-friendly
+- Progressive
+- Home-based if no equipment
+- Clear exercises + reps
+""",
+input_variables=["age", "goal", "equipment", "time"]
+)
+# fill the placeholders
+Workout_prompt = workout_prompt.invoke({
+    "age":age,
+    "goal":goal,
+    "equipment":equipment,
+    "time":time
+})
+calories = calorie_needs(weight, height, age, goal)
+diet_prompt = PromptTemplate(
+    template="""
+You are a certified Indian nutritionist.
+
+Create a daily meal plan:
+Diet type: {diet}
+Daily budget: ₹{budget}
+Target calories: {calories}
+
+Rules:
+- Indian meals only
+- Hostel-friendly
+- High protein
+- Affordable & practical
+""",
+input_variables=["diet", "budget", "calories"]
+)
+Diet_prompt = diet_prompt.invoke({
+    "diet":diet,
+    "budget":budget,
+    "calories":calories
+})
+
+
+
+
+if st.button("Generate My Plan"):
+    bmi = calculate_bmi(weight, height)
+    
+
+    workout = workout_model.invoke(Workout_prompt)
+
+    diet_plan = diet_model.invoke(Diet_prompt)
+
+    st.subheader("📊 Your Stats")
+    st.write(f"**BMI:** {bmi}")
+    st.write(f"**Daily Calories Target:** {calories}")
+
+    st.subheader("🏋️ Workout Plan")
+    st.write(workout.content)
+
+    st.subheader("🍛 Diet Plan")
+    st.write(diet_plan.content)
